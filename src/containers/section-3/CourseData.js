687736b0eb07866373437c0dd40fd363fef97e5c
shabrina12 <shabbrina5@gmail.com>
2022-12-07 20:30:38 +0700

    edit section 3 course card

---
+++ src/containers/section-3/CourseData.js
@@ -0,0 +1,71 @@
+import {course1, course2, course3, course4} from '../../images';
+
+const Data = [
+
+    {
+        id: 1,
+        img: course4,
+        category: 'Photography',
+        title: 'Capture like a Pro Masterclass',
+        total: '103 Lessons',
+        time: '6h 25min'
+    },
+    {
+        id: 2,
+        img: course3,
+        category: 'Business',
+        title: 'Customer Service Training',
+        total: '35 Lessons',
+        time: '1h 30min'
+    },
+    {
+        id: 3,
+        img: course2,
+        category: 'Design',
+        title: 'Become a UX Designer The Ultimate Guide',
+        total: '75 Lessons',
+        time: '8h 32min' 
+    },
+    {
+        id: 4,
+        img: course1,
+        category:'Music',
+        title: 'Complete Guitar Lessons',
+        total: '102 Lessons',
+        time: '19h 30min'
+    },
+    {
+        id: 5,
+        img: course4,
+        category: 'Photography',
+        title: 'Capture like a Pro Masterclass',
+        total: '103 Lessons',
+        time: '6h 25min'
+    },
+    {
+        id: 6,
+        img: course3,
+        category: 'Business',
+        title: 'Customer Service Training',
+        total: '35 Lessons',
+        time: '1h 30min'
+    },
+    {
+        id: 7,
+        img: course2,
+        category: 'Design',
+        title: 'Become a UX Designer The Ultimate Guide',
+        total: '75 Lessons',
+        time: '8h 32min' 
+    },
+    {
+        id: 8,
+        img: course1,
+        category:'Music',
+        title: 'Complete Guitar Lessons',
+        total: '102 Lessons',
+        time: '19h 30min'
+    }
+];
+
+export default Data;
--- src/containers/section-3/Section-3.jsx
+++ src/containers/section-3/Section-3.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import {course1, course2, course3, course4, bookIcon, watchIcon} from '../../images';
+import {bookIcon, watchIcon} from '../../images';
 import SubHeading from '../../components/subHeading/SubHeading';
+import Data from './CourseData';
 import './section3.css'
 
-const CourseCard = ({img, category, title, total, time}) => {
+const CourseCard = Data.map(item => {
     return (
-        <div className='course-card'>
-            <img src={img} alt='img' />
-            <p>{category}</p>
-            <h2>{title}</h2>
+        <div className='course-card' key={item.id}>
+            <img src={item.img} alt='img' />
+            <p>{item.category}</p>
+            <h2>{item.title}</h2>
             <div className='icon-container'>
-                <img src={bookIcon} />
-                <p>{total}</p>
-                <img src={watchIcon} />
-                <p>{time}</p>
+                <img src={bookIcon} alt='icon' />
+                <p>{item.total}</p>
+                <img src={watchIcon} alt='watch' />
+                <p>{item.time}</p>
             </div>
         </div>
     )
-}
+})
 
 const Section3 = () => {
   return (
@@ -25,25 +26,18 @@ const Section3 = () => {
         <SubHeading subtitle='COURSE FEATURES' title='Featured Courses' desc='Pursue your interests at your own pace by choosing the course duration, number of lessons per week, and lectures per day' />
         <div className='links-course-container'>
             <ul>
-                <li><a href='' className='active'>All Courses</a></li>
-                <li><a href=''>Development</a></li>
-                <li><a href=''>Photography</a></li>
-                <li><a href=''>Design</a></li>
-                <li><a href=''>Marketing</a></li>
-                <li><a href=''>Music</a></li>
-                <li><a href=''>Business</a></li>
+                <li><a href='#' className='active'>All Courses</a></li>
+                <li><a href='#'>Development</a></li>
+                <li><a href='#'>Photography</a></li>
+                <li><a href='#'>Design</a></li>
+                <li><a href='#'>Marketing</a></li>
+                <li><a href='#'>Music</a></li>
+                <li><a href='#'>Business</a></li>
             </ul>
         </div>
         <div className='container'>
             <div className='courses-container'>
-                <CourseCard img={course4} category='Photography' title='Capture like a Pro Masterclass' total='103 Lessons ' time='6h 25min' />
-                <CourseCard img={course3} category='Business' title='Customer Service Training' total='35 Lessons' time='1h 30min' />
-                <CourseCard img={course2} category='Design' title='Become a UX Designer The Ultimate Guide' total='75 Lessons' time='8h 32min' />
-                <CourseCard img={course1} category='Music' title='Complete Guitar Lessons' total='102 Lessons' time='19h 30min' />
-                <CourseCard img={course4} category='Photography' title='Capture like a Pro Masterclass' total='103 Lessons ' time='6h 25min' />
-                <CourseCard img={course3} category='Business' title='Customer Service Training' total='35 Lessons' time='1h 30min' />
-                <CourseCard img={course2} category='Design' title='Become a UX Designer The Ultimate Guide' total='75 Lessons' time='8h 32min' />
-                <CourseCard img={course1} category='Music' title='Complete Guitar Lessons' total='102 Lessons' time='19h 30min' />
+                {CourseCard}
             </div>
             <button type='button'>See All</button> 
         </div>
